c895560a6f30db88f08ba821921032023849566e
Abdullah Mamji <117278033+Abdullahmamji@users.noreply.github.com>
2022-11-09 04:51:10 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// var a = ["orange","apple","cherry","banana","grapes"]
+
+// a [6] = "mango"
+
+
+// a.push (banana)
+// a.pop()
+
+// a.shift()
+// a.unshift("apple")
+
+
+// var b = a.slice(0,2)
+// console.log (b);
+
+// var a = []
+
+// for (var i = 0; i < 10; i++){
+//     a.push("loop successfully complete" +i);
+// }
+// console.log (a);
+
+// var a = ["abc","def","ghi","jkl","mno","pqr"];\
+
+// for (var i = 0; i < a.length; i++){
+//     console.log(a[i]);
+// }
+
+
